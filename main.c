606834f98e235cdc8cf48cbad45d6c7ca3bf230f
Dana Ponce <katea9204@gmail.com>
2019-11-16 20:16:57 -0600

    Practica15

--- main.c
+++ main.c
@@ -4,46 +4,61 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-void lectura(archivo);
 int main(int argc, char *argv[]) {
-	FILE *archivo;
+	int i, j, lon, suma, a, b;
+	FILE *Archivo;
+	FILE *entrada;
+	FILE *Archivo_alreves;
+	char cad[100];
+	char inv[100];
 
-	char cad[100], inv[100];
+	Archivo=fopen("Archivo.txt", "w");
 
-	archivo=fopen("archivoOriginal.txt", "w");
-
-	if(archivo != NULL)
+	if(Archivo != NULL)
  	{
  		printf("Ingrese una cadena:  ");
 		fflush(stdin);
 		gets(cad);
-		fprintf(archivo, "%s", cad);
+		fprintf(Archivo, "%s", cad);
 	}
 	else{
 		printf("Estoy entrando al else");
 		exit(1);
 	}
-fclose(archivo);
-
-invertir(cad);
+fclose(Archivo);
+Archivo_alreves=fopen("Archivo_alreves.txt","w");
+    char temporal[' '];
+	lon= strlen(cad);
+    for (i=lon; i>=0;i--){
+		for (j=0; j<1 ;j++){
+			temporal[j]= cad[i];
+			fflush(stdin);
+		}
+		printf ( "%s", temporal );
+		
+	fprintf(Archivo_alreves,"\n\n La palabra alreves es \n PROCESADO  %s",temporal);
+		
+	}
+	fclose(Archivo_alreves);
+printf("\n\n %s",cad);
 
 printf("\n\n\n");
+
+//***********actividad2*****
+ if ((entrada = fopen("Valores.txt", "r")) == NULL)
+      return EXIT_FAILURE;
+   
+      fscanf(entrada, "%d %d", &a, &b);
+      suma=a+b;
+         
+   fclose(entrada);
+   
+   printf("La suma de a y b es: %d\n", suma);
+   printf("\n\n\n");
 system("pause");
+
 return 0;
 }
 
-char invertir(char cad[100]) {
-	int i,j, longitud;
-    char temporal[' '];
-	longitud= strlen(cad);
-    for (i=longitud; i>=0;i--){
-	for (j=0; j<longitud ;j++) {
 
-	temporal[j]= cad[i];
-}
-printf ( "%s" , temporal );
-}
-printf("%s", cad);
-  return cad[100];
-}
 
